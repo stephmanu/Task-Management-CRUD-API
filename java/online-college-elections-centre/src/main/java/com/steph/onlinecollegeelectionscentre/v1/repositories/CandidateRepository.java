e50f50f2b6ab1f529468dbe71bbe00021994a3dc
Stephen <macmanustephen@gmail.com>
2024-05-19 18:18:29 +0000

    Delete java/online-college-elections-centre directory

--- java/online-college-elections-centre/src/main/java/com/steph/onlinecollegeelectionscentre/v1/repositories/CandidateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.steph.onlinecollegeelectionscentre.v1.repositories;
-
-import com.steph.onlinecollegeelectionscentre.v1.entities.Candidate;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CandidateRepository extends JpaRepository<Candidate, Long> {
-}
--- java/online-college-elections-centre/src/main/java/com/steph/onlinecollegeelectionscentre/v1/repositories/VotesRepository.java
+++
@@ -1,7 +0,0 @@
-package com.steph.onlinecollegeelectionscentre.v1.repositories;
-
-import com.steph.onlinecollegeelectionscentre.v1.entities.Vote;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VotesRepository extends JpaRepository<Vote, Long> {
-}
